d0927aa4c28106afd374ca2f7f3f12a72ac9038e
“sofiavolpedo” <sofiavolpedo@gmail.com>
2022-04-20 13:07:35 -0300

    3ro

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/* JQUERY
+para esperar q termine de cargar
+$(document).ready(function(){
+
+
+});
+
+RESUMIDO:
+$(function(){
+
+    $("p").css({"background-color": "red"});
+    $("button").click(function(){
+        alert("hola")
+    });
+
+
+
+
+});
+
+*/
+
+$(function(){
+
+    $("button").click(function(){
+        $("body").hide();
+    });
+
+
+
+
+});
